5badbd075cea014deaa19feaef973dbecdba3876
Paul Guerra <paulrguerra@gmail.com>
2018-04-07 11:36:19 -0400

    render a conversation list

--- src/components/conversations.component.jsx
+++ src/components/conversations.component.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ActiveConversations() {
+export function conversationsList(conversations, list) {
+  return list.map(index => (
+    <li key={conversations[index].id}>
+      <button>{conversations[index].name}</button>
+    </li>
+  ));
+}
+
+export default function ActiveConversations({ conversations, list }) {
   return (
     <div>
       active conversation list goes here
+      <ul>
+        {conversationsList(conversations, list)}
+      </ul>
     </div>
   );
 }
 
-ActiveConversations.propTypes = {};
+ActiveConversations.propTypes = {
+  list: PropTypes.array.isRequired,
+  conversations: PropTypes.object.isRequired,
+};
--- src/containers/conversations.container.jsx
+++ src/containers/conversations.container.jsx
@@ -3,7 +3,10 @@ import ConversationsComponent from '../components/conversations.component';
 
 // map state to a prop of the component
 function mapStateToProps(state) {
-  return {};
+  return {
+    conversations: state.conversations,
+    list: state.conversationsList,
+  };
 }
 
 // map actions you want a component to dispatch to a prop of the component
