1f1328c03b58d36061f22cebb4a6800493166a1c
Paul Guerra <paulrguerra@gmail.com>
2018-04-16 18:45:31 -0400

    chore is a callback

--- src/utils/__tests__/repeat.test.js
+++ src/utils/__tests__/repeat.test.js
@@ -6,67 +6,67 @@ import repeat from '../repeat';
 jest.mock('../async-job');
 
 describe('repeat', () => {
-  let chore = jest.fn();
+  let callback = jest.fn();
   let limit = 1;
   let wait = 500;
 
   beforeEach(() => {
-    chore.mockClear();
+    callback.mockClear();
     makeAsyncJob.mockClear();
   });
 
   it('returns an promise', () => {
-    let promise = repeat(chore, limit);
+    let promise = repeat(callback, limit);
     expect(promise).toBeInstanceOf(Promise);
   });
 
   it('calls makeAsyncJob', () => {
-    repeat(chore);
+    repeat(callback);
     expect(makeAsyncJob).toHaveBeenCalled();
   });
 
-  it('calls makeAsyncJob with a task that executes the chore', () => {
-    repeat(chore, limit);
+  it('calls makeAsyncJob with a task that invokes the callback', () => {
+    repeat(callback, limit);
     let { task } = makeAsyncJob.mock.calls[0][0];
     task();
-    expect(chore).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledTimes(1);
   });
 
   it('calls makeAsyncJob with a stop function that is based on the limit provided', () => {
     // count starts at 0. Setting limit to 0 would make a stop check return true
-    repeat(chore, 0);
+    repeat(callback, 0);
     let { stop } = makeAsyncJob.mock.calls[0][0];
     expect(stop()).toBe(true);
   });
 
   it('lets you customize the wait between repeated calls', () => {
-    repeat(chore, 0, wait);
+    repeat(callback, 0, wait);
     expect(makeAsyncJob).toHaveBeenLastCalledWith(expect.any(Object), wait);
   });
 
   it('passes the current count to the task', () => {
-    repeat(chore);
+    repeat(callback);
     let { task } = makeAsyncJob.mock.calls[0][0];
     task();
-    expect(chore).toHaveBeenCalledWith(0);
+    expect(callback).toHaveBeenCalledWith(0);
     task();
-    expect(chore).toHaveBeenCalledWith(1);
+    expect(callback).toHaveBeenCalledWith(1);
   });
 });
 
 describe('integration with makeAsyncJob', () => {
-  let chore = jest.fn();
+  let callback = jest.fn();
   beforeEach(() => {
-    chore.mockClear();
+    callback.mockClear();
   });
 
   it('invokes the callback the specified number of times, with resolve and reject functions', () => {
     makeAsyncJob.mockImplementationOnce(require.requireActual('../async-job').makeAsyncJob);
-    return repeat(chore, 3).then(() => {
-      expect(chore).toHaveBeenCalledTimes(3);
-      expect(chore).toHaveBeenCalledWith(0, expect.any(Function), expect.any(Function));
-      expect(chore).toHaveBeenCalledWith(1, expect.any(Function), expect.any(Function));
-      expect(chore).toHaveBeenCalledWith(2, expect.any(Function), expect.any(Function));
+    return repeat(callback, 3).then(() => {
+      expect(callback).toHaveBeenCalledTimes(3);
+      expect(callback).toHaveBeenCalledWith(0, expect.any(Function), expect.any(Function));
+      expect(callback).toHaveBeenCalledWith(1, expect.any(Function), expect.any(Function));
+      expect(callback).toHaveBeenCalledWith(2, expect.any(Function), expect.any(Function));
     });
   });
 });
--- src/utils/repeat.js
+++ src/utils/repeat.js
@@ -1,9 +1,9 @@
 import { makeAsyncJob } from './async-job';
 
-export default function repeat(chore, limit = Infinity, wait = 0) {
+export default function repeat(callback, limit = Infinity, wait = 0) {
   let count = 0;
   const task = (...args) => {
-    chore(count, ...args);
+    callback(count, ...args);
     count += 1;
   };
   const stop = () => count === limit;
