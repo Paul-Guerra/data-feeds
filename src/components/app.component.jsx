5e4e8e9c7b33310161a0db5ac6253f82a2a3b7b0
Paul Guerra <paulrguerra@gmail.com>
2018-04-25 09:23:43 -0400

    add timer page

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -6,6 +6,7 @@ import {
   Link
 } from 'react-router-dom';
 import Home from '../components/home.component';
+import TimerPage from './timer-page.component';
 import SubscriptionsPage from './subscriptions-page.component';
 import ConversationsPage from './conversations-page.component';
 
@@ -20,11 +21,13 @@ export default class App extends React.Component {
         <div>
           <ul>
             <li><Link to="/">Home</Link></li>
+            <li><Link to="/timer">Timer</Link></li>
             <li><Link to="/subscriptions">Subscriptions</Link></li>
             <li><Link to="/conversations">Conversations</Link></li>
           </ul>
           <hr />
           <Route exact path="/" component={Home} />
+          <Route path="/timer" component={TimerPage} />
           <Route path="/subscriptions" component={SubscriptionsPage} />
           <Route path="/conversations" component={ConversationsPage} />
         </div>
